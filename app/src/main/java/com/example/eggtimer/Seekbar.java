4f75f18203932d2b5d91d873442d2b42369be2b4
moumita <mdas.160395@gmail.com>
2020-06-02 12:53:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/eggtimer/Seekbar.java
@@ -0,0 +1,18 @@
+package com.example.eggtimer;
+
+import android.widget.SeekBar;
+
+class Seekbar {
+    public Object setOnSeekBarChangeListener;
+    public Object setMax;
+    public Object setProgress;
+
+    public void setMax(int max) {
+    }
+
+    public void setProgress(float v) {
+    }
+
+    public void setOnSeekBarChangeListener(SeekBar.OnSeekBarChangeListener onSeekBarChangeListener) {
+    }
+}
